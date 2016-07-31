a37af77ec3246550a44c2f02c3bef0b0ff782e63
zhaotao <zhaotao@unipus.cn>
2016-07-31 16:43:30 +0800

    add #71, #72, #76,#79,#82

---
+++ algorithm/src/com/maginazt/Number72.java
@@ -0,0 +1,32 @@
+package com.maginazt;
+
+import java.util.Arrays;
+
+/**
+ * Created by zhaotao on 16/7/30.
+ */
+public class Number72 {
+
+    public int minDistance(String word1, String word2) {
+        if("".equals(word1) || "".equals(word2))
+            return Math.max(word1.length(), word2.length());
+        int len1 = word1.length();
+        int len2 = word2.length();
+        int[][] minD = new int[len1+1][len2+1];
+        minD[len1][len2] = 0;
+        //fill the corner cases
+        for(int row=len1-1;row>=0;row--)
+            minD[row][len2] = len1 - row;
+        for(int col=len2-1;col>=0;col--)
+            minD[len1][col] = len2 - col;
+        for(int row=len1-1;row>=0;row--)
+            for(int col=len2-1;col>=0;col--){
+                minD[row][col] = Math.min(1+minD[row+1][col], Math.min(1+minD[row][col+1], minD[row+1][col+1]+(word1.charAt(row) == word2.charAt(col) ? 0 : 1)));
+            }
+        return minD[0][0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Number72().minDistance("ache", "chchad"));
+    }
+}
---
+++ algorithm/src/com/maginazt/Number76.java
@@ -0,0 +1,77 @@
+package com.maginazt;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by zhaotao on 16/7/30.
+ */
+public class Number76 {
+
+    private static final Map<Character, Integer> patternMap = new HashMap<>();
+    private static final Map<Character, Integer> currentMap = new HashMap<>();
+    private static final Map<Character, Integer> clearMap = new HashMap<>();
+
+    public String minWindow(String s, String t) {
+        int minWindowStart = 0;
+        int minWindowLength = Integer.MAX_VALUE;
+        String result = "";
+        patternMap.clear();
+        currentMap.clear();
+        clearMap.clear();
+        for(int i=0;i<t.length();i++){
+            Character key = t.charAt(i);
+            if(patternMap.containsKey(key))
+                patternMap.put(key, patternMap.get(key)+1);
+            else
+                patternMap.put(key, 1);
+        }
+        clearMap.putAll(patternMap);
+        int index = 0;
+        while(index < s.length()){
+            Character ch = s.charAt(index);
+            if(patternMap.containsKey(ch)){
+                if(currentMap.containsKey(ch))
+                    currentMap.put(ch, currentMap.get(ch)+1);
+                else
+                    currentMap.put(ch, 1);
+                if(!clearMap.isEmpty()){
+                    if(clearMap.containsKey(ch) && clearMap.get(ch) > 1)
+                        clearMap.put(ch, clearMap.get(ch)-1);
+                    else
+                        clearMap.remove(ch);
+                }
+                if(clearMap.isEmpty()){
+                    //adjust the window start index
+                    while (true){
+                        Character lft = s.charAt(minWindowStart);
+                        if(!patternMap.containsKey(lft)){
+                            ++minWindowStart;
+                            continue;
+                        }
+                        else if(currentMap.get(lft) > patternMap.get(lft)){
+                            currentMap.put(lft, currentMap.get(lft)-1);
+                            ++minWindowStart;
+                            continue;
+                        }
+                        else
+                            break;
+                    }
+                    int length = index - minWindowStart + 1;
+                    if(length < minWindowLength){
+                        minWindowLength = length;
+                        result = s.substring(minWindowStart, minWindowStart+minWindowLength);
+                    }
+                }
+            }
+            ++index;
+        }
+        if(!clearMap.isEmpty())
+            return "";
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Number76().minWindow("cabwefgewcwaefgcf", "cae"));
+    }
+}
